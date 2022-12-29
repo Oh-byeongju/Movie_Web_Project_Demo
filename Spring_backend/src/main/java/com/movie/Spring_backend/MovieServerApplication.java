d109c052274fb029c621499c3056f4e66672efe9
Oh-byeongju <dhqudwn0@naver.com>
2022-12-29 22:32:18 +0900

    TopButton 생성

---
+++ Spring_backend/src/main/java/com/movie/Spring_backend/MovieServerApplication.java
@@ -0,0 +1,14 @@
+package com.movie.Spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieServerApplication.class, args);
+		System.out.println("껄껄");
+	}
+
+}
