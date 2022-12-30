9ba308169d6e661a1e28e4682ba8e67285ec99bf
kmsjkh12 <kmsjkh12@naver.com>
2022-12-30 23:05:13 +0900

    배너, 캐러셀

--- React_frontend/src/pages/Home.js
+++ React_frontend/src/pages/Home.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import TopButtons from '../Common_components/TopButtons';
-import {createGlobalStyle} from 'styled-components';
+import React from "react";
+import TopButtons from "../Common_components/TopButtons";
+import { createGlobalStyle } from "styled-components";
+import BoxList from "../boxoffice/BoxList";
+import HomeBanner from "../Common_components/HomeBanner";
 
 const Home = () => {
-	return (
-		<>
-			<GlobalStyle/>
-			<TopButtons/>
-		</>
-	);
+  return (
+    <>
+      <GlobalStyle />
+      <TopButtons />
+      <HomeBanner />
+      <BoxList />
+    </>
+  );
 };
-
 
 const GlobalStyle = createGlobalStyle`
   body {
