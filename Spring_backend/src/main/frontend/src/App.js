c7a960e9258d4f32e4749bb60000fc08e6d95358
Oh-byeongju <dhqudwn0@naver.com>
2022-12-31 00:49:11 +0900

    server upload

---
+++ Spring_backend/src/main/frontend/src/App.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
--- Spring_backend/src/main/java/com/movie/Spring_backend/MovieServerApplication.java
+++ Spring_backend/src/main/java/com/movie/Spring_backend/MovieServerApplication.java
@@ -2,10 +2,10 @@ package com.movie.Spring_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 public class MovieServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MovieServerApplication.class, args);
 		System.out.println("껄껄");
