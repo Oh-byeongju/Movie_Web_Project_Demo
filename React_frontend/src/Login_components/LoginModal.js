4945158e8d85b6f285607d251c59ab3722df9b66
kmsjkh12 <kmsjkh12@naver.com>
2023-01-03 19:13:06 +0900

    주석 추가

--- React_frontend/src/Login_components/LoginModal.js
+++ React_frontend/src/Login_components/LoginModal.js
@@ -4,7 +4,8 @@ import { CloseOutlined } from "@ant-design/icons";
 import { Login_reduecer } from "../reducer/user";
 import { useDispatch } from "react-redux";
 const LoginModal = ({ setlogin }) => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); //useDispatch를 dispatch로 선언
+
   // x버튼 클릭시 setlogin을 false로 해서 모달창을 닫음
   const closeLogin = () => {
     setlogin(false);
@@ -28,7 +29,7 @@ const LoginModal = ({ setlogin }) => {
   // 로그인 버튼 누를 때 적용되는 함수
   const submit = () => {
     console.log(id, pw);
-    dispatch(Login_reduecer({ id, pw }));
+    dispatch(Login_reduecer({ id, pw })); // 로그인 버튼 눌리면 리듀서에서 만든 Login_reducer 함수 실행 { } 안에 넣는 이유는 두개 이상의 데이터일 때
   };
 
   // id, pw 입력에 따른 로그인 버튼 활성화 함수
--- React_frontend/src/reducer/index.js
+++ React_frontend/src/reducer/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
-
+import ticket from "./ticket";
 import user from "./user";
 const rootReducer = combineReducers({
   user,
+  ticket,
 });
+//리듀서 파일 생성 시 여기 추가하면 됨.
 
 export default rootReducer;
