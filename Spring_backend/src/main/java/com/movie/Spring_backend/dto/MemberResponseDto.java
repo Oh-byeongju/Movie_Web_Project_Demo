8768e51f8e900f82ea9bfb4e869844b66f8fc2dd
Oh-byeongju <dhqudwn0@naver.com>
2023-01-16 02:17:52 +0900

    스프링 시큐리티 작업

--- Spring_backend/src/main/java/com/movie/Spring_backend/dto/MemberResponseDto.java
+++ Spring_backend/src/main/java/com/movie/Spring_backend/dto/MemberResponseDto.java
@@ -1,5 +1,5 @@
 // 23-01-16 회원가입 성공시 전달할 ResponseDto 생성(오병주)
-// 회원가입 시 일단 임의로 보내주는 Dto 추후 삭제 할수도?
+// 회원가입 시 일단 임의로 보내주는 Dto 추후 삭제 할수도??
 package com.movie.Spring_backend.dto;
 
 import lombok.Builder;
