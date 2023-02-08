3fb929449f9024a3ec16453537073796dfca51a2
Oh-byeongju <dhqudwn0@naver.com>
2023-02-09 02:27:51 +0900

    내림차순 정렬 적용

--- Spring_backend/src/main/java/com/movie/Spring_backend/repository/MovieRepository.java
+++ Spring_backend/src/main/java/com/movie/Spring_backend/repository/MovieRepository.java
@@ -1,22 +1,36 @@
+/*
+    23-02-09 JPQL을 이용하여 좋아요 순으로 영화 조회 하도록 재설계(오병주)
+ */
+
 package com.movie.Spring_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.movie.Spring_backend.entity.MovieEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<MovieEntity,Long> {
-    List<MovieEntity> findAll();
+
+    // 모든 영화 조회 메소드(좋아요 순으로 내림차순 --> 나중에 예매율로 바꿔야함)
+    @Query(value = "SELECT m FROM MovieEntity as m " +
+                   "ORDER BY m.cntMovieLike DESC")
+    List<MovieEntity> findAllDESC();
+
+    // 사용자가 영화 검색시 조회 메소드(좋아요 순으로 내림차순 --> 나중에 예매율로 바꿔야함)
+    @Query(value = "SELECT m FROM MovieEntity as m " +
+                   "WHERE m.mtitle LIKE CONCAT('%',:title,'%') " +
+                   "ORDER BY m.cntMovieLike DESC")
+    List<MovieEntity> findSearchDESC(@Param("title") String title);
 
     //단순 영화 검색
-    public List<MovieEntity> findByMid(Long id);
+    List<MovieEntity> findByMid(Long id);
 
     //극장 클릭 시 영화 id list를 활용하여 검색
-    public List<MovieEntity> findByMidIn(List<Long> mid);
+    List<MovieEntity> findByMidIn(List<Long> mid);
 
-    //Containing == select시 like할때 사용
-    public List<MovieEntity> findByMtitleContaining(String title);
+
 }
