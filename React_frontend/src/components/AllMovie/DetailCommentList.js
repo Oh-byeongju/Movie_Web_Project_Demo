67e8d91ec0b3ae7a1bae2fa014f7132ab21ef387
Oh-byeongju <dhqudwn0@naver.com>
2023-02-19 02:20:37 +0900

    관람평 수정

--- React_frontend/src/components/AllMovie/DetailCommentList.js
+++ React_frontend/src/components/AllMovie/DetailCommentList.js
@@ -1,22 +1,57 @@
 /*
- 23-02-16 영화 상세정보 댓글창 구현(오병주)
+  23-02-16 영화 상세정보 댓글창 구현(오병주)
+	23-02-18 영화 상세정보 댓글창 분리(오병주)
 */
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Rate } from 'antd';
-import { StarFilled, SmileFilled, MehFilled, FrownFilled, DeleteOutlined, LikeOutlined } from "@ant-design/icons";
+import { StarFilled, UnorderedListOutlined, LikeOutlined } from "@ant-design/icons";
+import DetailComment from './DetailComment';
+import { useCallback } from 'react';
 
 const DetailCommentList = () => {
 
-	const [data, setdata] = useState({
-		id: 'temp1',
-		comment: '너무 재밌네요 너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요',
-		date: '2023-02-16',
-		score: '0',
-		up_cnt: '7',
-	});
+	// const datas = ['']
+	const datas = [
+		{
+			id: 'temp1',
+			comment: '너무 재밌네요 너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요',
+			date: '2023-02-16',
+			score: '7',
+			up_cnt: '7',
+		},
+		{
+			id: 'temp2',
+			comment: '너무 재밌네요 너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요',
+			date: '2023-02-16',
+			score: '3',
+			up_cnt: '5',
+		},
+		{
+			id: 'temp3',
+			comment: '너무 재밌네요 너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌123123123네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요너무 재밌네요',
+			date: '2023-02-16',
+			score: '5',
+			up_cnt: '1',
+		}
+	];
 
-	console.log(data.id)
+	// 관람평 더보기 limit 이거 조금 더 고민해보기
+	const [limit, setlimit] = useState(6); 
+	const L = 8;
+
+	// 정렬 버튼 css 변수
+	const [newbutton, setnewbutton] = useState(true);
+	const [likebutton, setlikebutton] = useState(false);
+
+	const clicknew = useCallback(()=> {
+		setnewbutton(true);
+		setlikebutton(false);
+	}, [])
+
+	const clicklike = useCallback(()=> {
+		setlikebutton(true);
+		setnewbutton(false);
+	}, [])
 
 	return (
 		<Layout>
@@ -27,71 +62,35 @@ const DetailCommentList = () => {
 				<CommentHeader>
 					<div className='StarCheck'>
 						<span>
-							<StarFilled style={{color:"#fea408"}}/> 관람평점
+							<StarFilled style={{color:"#fea408"}}/> &nbsp;관람객 평점
 						</span>
 					</div>
-					<div>
-						<span>
-							관람평 178개
+					<div className='right'>
+						<span className='cnt_num'>
+							관람평
+							<strong> 178</strong>
+							개
 						</span>
-						<button>
-							최신순
-						</button>
-						<button>
-							추천순
-						</button>
+						<ButtonList>
+							<ButtonWrap>
+								<button className={"btn" + (newbutton ? " active" : "")} onClick={clicknew}>
+									<UnorderedListOutlined/> 최신순
+								</button>
+							</ButtonWrap>
+							<ButtonWrap>
+								<button style={{marginLeft: "7px"}} className={"btn" + (likebutton ? " active" : "")} onClick={clicklike}>
+									<LikeOutlined/> 공감순
+								</button>
+							</ButtonWrap>
+						</ButtonList>
 					</div>
 				</CommentHeader>
 				<CommentList>
-					<CommentElement>
-						<span className='img'>
-							{data.up_cnt > 6 ? <SmileFilled style={{fontSize: "40px", color: "#4a4321"}}/> 
-							: data.up_cnt > 3 ? <MehFilled style={{fontSize: "40px", color: "#4a4321"}} /> : <FrownFilled style={{fontSize: "40px", color: "#4a4321"}}/>}
-						</span>
-						<div className='top'>
-							<span className='name'>
-								{data.id}
-							</span>
-							<span className='score'>
-								<RateCustom allowHalf value={data.up_cnt/2}/> 
-								<span>
-									{data.up_cnt}
-								</span>
-							</span>
-							<span className='date'>
-								{data.date}
-							</span>
-						</div>
-						<div className='middle'>
-							{data.comment}
-						</div>
-						<div className='bottom'>
-							<span>
-								&nbsp;
-							</span>
-							
-							<span className='cnt'>
-								{data.up_cnt}
-							</span>
-							<ButtonCustom>
-								<LikeOutlined/>
-							</ButtonCustom>
-							
-							<ButtonCustom>
-								<DeleteOutlined/>
-							</ButtonCustom>
-							
-						</div>
-					</CommentElement>
-					<CommentElement>
-						2
-					</CommentElement>
-					<CommentElement>
-						3
-					</CommentElement>
+					{datas.map((data, index) => (
+    				<DetailComment data={data} key={index} />
+  				))}
 				</CommentList>
 			</CommentSection>
-			
 		</Layout>
 	);
 };
@@ -116,7 +115,7 @@ const CommentSection = styled.div`
 	width: 1050px;
 	margin-top: 8px;
 	background-color: #fff;
-	box-shadow: 0 1px 3px 0 rgb(0 0 0 / 15%);
+	box-shadow: 0px 1px 4px 0px rgb(0 0 0 / 15%);
 
 	h4 {
 		padding-top: 18px;
@@ -131,93 +130,82 @@ const CommentHeader = styled.div`
 	padding: 16px;
 	display: flex;
 	justify-content: space-between;
+
+	.StarCheck {
+		color: #999;
+		font-size: 14px;
+		margin-left: 14px;
+		padding-top: 5px;
+	}
+
+	.right {
+		display: flex;
+    align-items: center;
+    margin-left: auto;
+
+		.cnt_num {
+			font-size: 14px;
+		}
+	}
+`;
+
+const ButtonList = styled.ul`
+	position: relative;
+	margin-left: 5px !important;
+	list-style: none;
+	margin: 0;
+	padding: 0;
+	margin-right: 11px;
+	margin-top: 1px;
+
+	::before{
+		content: '';
+    display: block;
+    position: absolute;
+    left: 5px;
+    top: 2px;
+    width: 1px;
+    height: 16px;
+    background-color: #ccc;
+	}
+
+	li:first-child {
+		margin-left: 15px;
+	}
+
+`;
+
+const ButtonWrap = styled.li`
+	margin-left: 10px;
+	list-style: none;
+	display: list-item;
+	float: left;
+
+	.btn {
+		content: "";
+		cursor: pointer;
+		background-color: white;
+		display: block;
+		position: relative;
+		color: #999;
+		font-size: 14px;
+		border: 0;
+		padding: 0;
+
+		&.active {
+      color: #000;
+    }
+	}
 `;
 
 const CommentList = styled.ul`
-	padding: 25px;
+	padding: 0px 25px 0px 25px;
 	background: #f8f8f8;
 	margin: 0 auto;
 	margin-bottom: 8px;
 	background-color: #fff;
-	box-shadow: 0 1px 3px 0 rgb(0 0 0 / 15%);
-`;
 
-const CommentElement = styled.li`
-	position: relative;
-	padding: 20px 0 15px 68px;
-	border-top: 1px solid #eee;
-	list-style: none;
-
-	.img {
-		display: block;
-    position: absolute;
-    top: 20px;
-    left: 10px;
-    width: 42px;
-    height: 42px;
+	li:first-child {
+		border-color: #ccc;
 	}
-
-	.top {
-		position: relative;
-    margin-bottom: 6px;
-
-		.name {
-			display: block;
-			font-size: 14px;
-			padding-bottom: 1px;
-		}
-
-		.score {
-			position: relative;
-			font-size: 14px;
-    	margin-right: 11px;
-		}
-
-		.date {
-			position: absolute;
-			top: 0;
-			right: 10px;
-			font-family: "Roboto";
-			font-size: 14px;
-			color: #999;
-		}
-	}
-
-	.middle {
-		margin-bottom: 0;
-    line-height: 20px;
-		font-size: 13px;
-	}
-
-	.bottom {
-		margin-top: 8px;
-		margin-bottom: 0;
-    line-height: 20px;
-    font-size: 13px;
-
-		.cnt {
-			float: right;
-			padding-right: 7px;
-			vertical-align: top;
-		}
-	}
-`;
-
-const RateCustom = styled(Rate)`
-	pointer-events: none;
-	color: #fea408;
-	font-size: 16px;
-
-	.ant-rate-star{
-		margin-inline-end: 2px !important;
-	}
-`;
-
-const ButtonCustom = styled.button`
-	float: right;
-  content: "";
-  cursor: pointer;
-  background-color: white;
-  border: 0;
-	margin-right: 5px;
 `;
