0ab10c6db017008c59cefe06f1b1cc2db6c60cac
kmsjkh12 <kmsjkh12@naver.com>
2023-04-09 00:55:12 +0900

    push

---
+++ React_frontend/src/pages/BoardEdit.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Edit from "../components/Board/Edit";
+const BoardEdit = () =>{
+    return(
+            <>
+            <Edit />
+            </>
+    )
+}
+export default BoardEdit;
